64296c5fd0be713ae994ad1d0fadf295a59e9ba5
juliettehosch <144881835+juliettehosch@users.noreply.github.com>
2023-09-13 14:33:21 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  const chatContent = document.getElementById("chat-content");
+  const messageArea = document.getElementById("messageArea");
+
+  messageArea.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const userMessage = document.getElementById("text").value;
+
+});
+
+});
